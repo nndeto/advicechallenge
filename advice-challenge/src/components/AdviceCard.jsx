b06c0704b2a448148894d94332f00f7b96b00a33
nndeto <86444679+nndeto@users.noreply.github.com>
2023-11-07 04:11:28 -0500

    coded api call to advice app

--- advice-challenge/src/components/AdviceCard.jsx
+++ advice-challenge/src/components/AdviceCard.jsx
@@ -1,14 +1,34 @@
 import { useState, useEffect } from "react"
 import axios from 'axios';
+import '../css/style.css'
 
 function AdviceCard() {
+        const [advice, setAdvice] = useState("There is none")
+
+        useEffect(() => {
+            axios
+                .get("https://api.adviceslip.com/advice")
+                .then((response) => {
+                    let adviceSlip = response.data.slip
+                    // console.log(response.data.slip.advice)
+                    setAdvice(adviceSlip.advice);
+
+                })
+                .catch((error) => {
+                    console.error(error);
+                })
+        })
 
 
-    <>
-        <div>
-            <p>device card verbal vomit</p>
-        </div>
-    </>
+    return(
+        <>
+            <div class = "container">
+                <p>{advice}</p>
+            </div>
+        </>
+
+    )
+
 }
 
 export default AdviceCard
