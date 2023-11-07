2e48c2f1c9cc8109ee9b6a244ddd0ea15ea80994
nndeto <86444679+nndeto@users.noreply.github.com>
2023-11-07 02:43:50 -0500

    switched vanilla app to be a react app

---
+++ advice-challenge/src/App.jsx
@@ -0,0 +1,18 @@
+import "./css/style.css"
+
+function App() {
+
+  return (
+    <>
+      <footer>
+        <div >
+          Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+          Coded by <a href="https://builtbynicole.dev">Nicole Ndeto</a>.
+        </div>
+      </footer>
+        
+    </>
+  )
+}
+
+export default App
